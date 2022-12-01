95cabc9e73470bf305682ce09406ad16a2bd6ecb
Paulius Vizbara <paulius1207@gmail.com>
2022-12-01 16:10:39 +0200

    Working API

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,9 +1,10 @@
 import fetch from 'node-fetch';
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 export const getInput = async (day: number) => {
-    const url = `https://adventofcode.com/2021/day/${day}/input`
-    const data = await fetch(url)
-    console.log(data);
+    const url = `https://adventofcode.com/2022/day/${day}/input`
+    const res = await fetch(url, { headers: { 'Cookie': `session=${process.env['SESSION']}` } })
+    const data = await (await res.text()).split('\n')
+    return data
 }
-
-getInput(1)
