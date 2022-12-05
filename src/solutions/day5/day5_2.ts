e22abf1645ca710ae9a4e61a0962a34591676fc4
Paulius Vizbara <paulius1207@gmail.com>
2022-12-05 10:19:34 +0200

    Day 5 solved

---
+++ src/solutions/day5/day5_2.ts
@@ -0,0 +1,28 @@
+
+import { getInput } from "../../api/api.js";
+
+export default async () => {
+
+    const data = (await getInput(5, '\n'))
+    const stacksInput = data.slice(0, data.indexOf(''))
+    const stacksIndices = stacksInput.pop()
+    const stacksAmount = +stacksIndices[stacksIndices.length - 2]
+    const stacks = Array.from(Array(stacksAmount), () => new Array());
+    const instructions = data.slice(data.indexOf('') + 1, data.length - 1)
+    for (let i = stacksInput.length - 1; i >= 0; i--) {
+        const stackInput = stacksInput[i]
+        for (let j = 1; j < stackInput.length; j += 4) {
+            stackInput[j] !== ' ' && stacks[+stacksIndices[j] - 1].push(stackInput[j])
+        }
+    }
+    instructions.forEach(instruction => {
+        const [, amount, , from, , to] = instruction.split(' ')
+        const fromIndex = +from - 1
+        const toIndex = +to - 1
+
+        const movedCrates = stacks[fromIndex].splice(stacks[fromIndex].length - +amount)
+        stacks[toIndex].push(...movedCrates)
+    })
+    const cratesOnTop = stacks.reduce((acc, curr) => acc + curr[curr.length - 1], '')
+    console.log(cratesOnTop);
+}
---
+++ src/solutions/day5/index.ts
@@ -0,0 +1,4 @@
+import day5_1 from './day5_1.js';
+import day5_2 from './day5_2.js'
+
+day5_2()
