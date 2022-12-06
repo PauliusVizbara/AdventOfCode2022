d2303cde2981eab3f8d1cc231922bb80b144d9e3
Paulius Vizbara <paulius1207@gmail.com>
2022-12-06 14:56:53 +0200

    Day 6 part 1 solved

---
+++ src/solutions/day6/day6_1.ts
@@ -0,0 +1,20 @@
+
+import { getInput } from "../../api/api.js";
+
+export default async () => {
+
+    const data = (await getInput(6, '\n')).shift()
+
+    const packetSize = 4
+    let marker = 0
+    for (let i = packetSize; i < data.length; i++) {
+        const packet = data.slice(i - packetSize, i)
+        const uniqueCharactersAmount = new Set(packet).size
+        if (uniqueCharactersAmount === packetSize) {
+            marker = i;
+            break
+        }
+
+    }
+    console.log(marker);
+}
---
+++ src/solutions/day6/index.ts
@@ -0,0 +1,4 @@
+import day6_1 from './day6_1.js';
+import day6_2 from './day6_2.js'
+
+day6_1()
