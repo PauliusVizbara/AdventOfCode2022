5c830fcd3486af7c68bd137037af01f589e0b632
Paulius Vizbara <paulius1207@gmail.com>
2022-12-06 19:32:28 +0200

    Day 6 part 2 solved

--- src/solutions/day6/day6_2.ts
+++ src/solutions/day6/day6_2.ts
@@ -3,5 +3,18 @@ import { getInput } from "../../api/api.js";
 
 export default async () => {
 
-    const data = (await getInput(6, '\n'))
+    const data = (await getInput(6, '\n')).shift()
+
+    const packetSize = 14
+    let marker = 0
+    for (let i = packetSize; i < data.length; i++) {
+        const packet = data.slice(i - packetSize, i)
+        const uniqueCharactersAmount = new Set(packet).size
+        if (uniqueCharactersAmount === packetSize) {
+            marker = i;
+            break
+        }
+
+    }
+    console.log(marker);
 }
--- src/solutions/day6/index.ts
+++ src/solutions/day6/index.ts
@@ -1,4 +1,4 @@
 import day6_1 from './day6_1.js';
 import day6_2 from './day6_2.js'
 
-day6_1()
+day6_2()
