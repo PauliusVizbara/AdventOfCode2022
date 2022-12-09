1e1657328904db310a7fb9bdcb51dc21f53cefa4
Paulius Vizbara <paulius1207@gmail.com>
2022-12-09 06:59:34 +0200

    Day 8 part 2

--- src/solutions/day8/day8_2.ts
+++ src/solutions/day8/day8_2.ts
@@ -3,11 +3,12 @@ import { getInput } from "../../api/api.js";
 
 const visibilityToLeft = (data: string[], i: number, j: number) => {
     const tree = data[i][j]
-    let visibility = 1
+    let visibility = 0
     let x = j;
+
     while (--x >= 0) {
         if (data[i][x] >= tree) {
-            return visibility
+            return visibility + 1
         }
         visibility++
     }
@@ -16,11 +17,12 @@ const visibilityToLeft = (data: string[], i: number, j: number) => {
 
 const visibilityToRight = (data: string[], i: number, j: number) => {
     const tree = data[i][j]
-    let visibility = 1
+    let visibility = 0
     let x = j;
+
     while (++x <= data.length - 1) {
         if (data[i][x] >= tree) {
-            return visibility
+            return visibility + 1
         }
         visibility++
     }
@@ -30,12 +32,12 @@ const visibilityToRight = (data: string[], i: number, j: number) => {
 const visibilityToTop = (data: string[], i: number, j: number) => {
 
     const tree = data[i][j]
-    let visibility = 1
+    let visibility = 0
 
     let y = i;
     while (--y >= 0) {
         if (data[y][j] >= tree) {
-            return visibility
+            return visibility + 1
         }
         visibility++
     }
@@ -44,12 +46,13 @@ const visibilityToTop = (data: string[], i: number, j: number) => {
 
 const visibilityToBottom = (data: string[], i: number, j: number) => {
     const tree = data[i][j]
-    let visibility = 1
+    let visibility = 0
 
     let y = i;
+
     while (++y <= data.length - 1) {
         if (data[y][j] >= tree) {
-            return visibility
+            return visibility + 1
         }
         visibility++
     }
@@ -58,18 +61,20 @@ const visibilityToBottom = (data: string[], i: number, j: number) => {
 
 export default async () => {
 
-    // const data = (await getInput(8, '\n')).filter(row => row !== '')
+    const data = (await getInput(8, '\n')).filter(row => row !== '')
 
-    const data = [
-        '30373',
-        '25512',
-        '65332',
-        '33549',
-        '35390',
-    ]
+    // const data = [
+    //     '30373',
+    //     '25512',
+    //     '65332',
+    //     '33549',
+    //     '35390',
+    // ]
 
 
     let visibleTrees = data.length * 2 + (data[0].length - 2) * 2
+
+    let max = 0;
 
     for (let i = 1; i < data.length - 1; i++) {
         for (let j = 1; j < data[0].length - 1; j++) {
@@ -87,14 +92,16 @@ export default async () => {
             const topVis = visibilityToTop(data, i, j)
             const bottomVis = visibilityToBottom(data, i, j)
 
+            const score = leftVis * rightVis * topVis * bottomVis
+            if (score > max) max = score
 
-            console.log(leftVis);
 
 
         }
 
     }
-    console.log(visibleTrees);
+    console.log(max);
+
 
 
 }
