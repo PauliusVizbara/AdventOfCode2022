44ca274f2b98188d84e4fe20ca2ee059aa347141
Paulius Vizbara <paulius1207@gmail.com>
2022-12-17 14:15:22 +0200

    Improve generation

--- src/utils/generation.ts
+++ src/utils/generation.ts
@@ -2,7 +2,9 @@ import { writeFileSync, mkdirSync } from 'fs'
 
 const day = process.argv[2]
 const indexContent = `import ${day}_1 from './${day}_1.js';
-import ${day}_2 from './${day}_2.js'`;
+import ${day}_2 from './${day}_2.js'
+
+${day}_1()`;
 
 const puzzleContent = `
 import { getInput } from "../../api/api.js";
