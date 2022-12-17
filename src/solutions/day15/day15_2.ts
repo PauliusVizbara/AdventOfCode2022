4806f9de278e9fc4136ae2ccae36cba3c757c34c
Paulius Vizbara <paulius1207@gmail.com>
2022-12-17 15:41:57 +0200

    WIP Day 15 part 2

--- src/solutions/day15/day15_2.ts
+++ src/solutions/day15/day15_2.ts
@@ -1,7 +1,69 @@
 
 import { getInput } from "../../api/api.js";
 
+type Point = {
+    x: number,
+    y: number
+}
+
+const distanceBetweenPoints = (point1: Point, point2: Point) => Math.abs(point1.x - point2.x) + Math.abs(point1.y - point2.y)
+
 export default async () => {
 
-    const data = (await getInput(15, '\n'))
-}
+    // const data = (await getInput(15, '\n'))
+    // data.pop()
+    const data = [
+        'Sensor at x=2, y=18: closest beacon is at x=-2, y=15',
+        'Sensor at x=9, y=16: closest beacon is at x=10, y=16',
+        'Sensor at x=13, y=2: closest beacon is at x=15, y=3',
+        'Sensor at x=12, y=14: closest beacon is at x=10, y=16',
+        'Sensor at x=10, y=20: closest beacon is at x=10, y=16',
+        'Sensor at x=14, y=17: closest beacon is at x=10, y=16',
+        'Sensor at x=8, y=7: closest beacon is at x=2, y=10',
+        'Sensor at x=2, y=0: closest beacon is at x=2, y=10',
+        'Sensor at x=0, y=11: closest beacon is at x=2, y=10',
+        'Sensor at x=20, y=14: closest beacon is at x=25, y=17',
+        'Sensor at x=17, y=20: closest beacon is at x=21, y=22',
+        'Sensor at x=16, y=7: closest beacon is at x=15, y=3',
+        'Sensor at x=14, y=3: closest beacon is at x=15, y=3',
+        'Sensor at x=20, y=1: closest beacon is at x=15, y=3',
+    ]
+
+    const locations: { sensor: Point, beacon: Point, distanceBetween: number }[] = []
+
+    const maxTestedCoordinate = 2e1
+    data.forEach(row => {
+        const [sensorString, beaconString] = row.split(':')
+        const sensorX = +sensorString.slice(sensorString.indexOf('x=') + 2, sensorString.indexOf(','))
+        const sensorY = +sensorString.slice(sensorString.indexOf('y=') + 2)
+        const beaconX = +beaconString.slice(beaconString.indexOf('x=') + 2, beaconString.indexOf(','))
+        const beaconY = +beaconString.slice(beaconString.indexOf('y=') + 2)
+        locations.push({ sensor: { x: sensorX, y: sensorY }, beacon: { x: beaconX, y: beaconY }, distanceBetween: distanceBetweenPoints({ x: sensorX, y: sensorY }, { x: beaconX, y: beaconY }) })
+    })
+
+    for (let x = 0; x < maxTestedCoordinate; x++) {
+        for (let y = 0; y < maxTestedCoordinate; y++) {
+
+            const testedPoint = { x, y }
+
+            let isValidLocation = true
+            for (const location of locations) {
+                if (distanceBetweenPoints(testedPoint, location.sensor) <= location.distanceBetween) {
+                    isValidLocation = false;
+                    break
+                }
+            }
+
+            if (isValidLocation) {
+                console.log(x, y);
+            }
+
+        }
+    }
+
+
+
+
+
+    console.log('Done');
+}   
--- src/solutions/day15/index.ts
+++ src/solutions/day15/index.ts
@@ -1,4 +1,4 @@
 import day15_1 from './day15_1.js';
 import day15_2 from './day15_2.js'
 
-day15_1()
+day15_2()
